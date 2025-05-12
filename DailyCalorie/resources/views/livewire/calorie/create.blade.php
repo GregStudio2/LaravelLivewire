4e8f55040240c931eb6e17ff256af8ff9a0feb22
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-05-12 20:28:45 +0700

    create new project dailycalorie

---
+++ DailyCalorie/resources/views/livewire/calorie/create.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <div class="card">
+        <div class="card-body">
+            <form wire:submit.prevent="store">
+                <div class="form-group">
+                    <label>NAME</label>
+                    <input type="text" wire:model="name" class="form-control @error('name') is-invalid @enderror" placeholder="Masukkan Name">
+                    @error('name')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>CALORIE</label>
+                   <textarea wire:model="calorie" class="form-control @error('calorie') is-invalid @enderror" rows="4" placeholder="Masukkan Calorie"></textarea>
+                   @error('calorie')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">SIMPAN</button>
+            </form>
+        </div>
+    </div>
+</div>
