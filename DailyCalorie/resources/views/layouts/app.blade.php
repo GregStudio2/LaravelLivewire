96d525af263df404279ff8d041e3b56eff174b88
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-05-13 03:32:39 +0700

    Update app.blade.php

--- DailyCalorie/resources/views/layouts/app.blade.php
+++ DailyCalorie/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Livewire SPA - SantriKoding.com</title>
+    <title>DailyCalorie</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <livewire:styles/>
     <livewire:scripts/>
