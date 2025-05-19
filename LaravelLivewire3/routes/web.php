b650c39ee37e461b11f95a45dccf8c5c399491ca
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-05-20 04:26:54 +0700

    create laravellivewire3

---
+++ LaravelLivewire3/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//posts index
+// Route::get('/', App\Livewire\Posts\Index::class)->name('posts.index');
+
+//posts create
+// Route::get('/create', App\Livewire\Posts\Create::class)->name('posts.create');
+
+//posts edit
+// Route::get('/edit/{id}', App\Livewire\Posts\Edit::class)->name('posts.edit');
