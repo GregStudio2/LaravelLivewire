7a561b742cfffb14cf354a820429d3d11f418de2
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-05-20 21:04:44 +0700

    Update app.blade.php

--- LaravelLivewire3/resources/views/layouts/app.blade.php
+++ LaravelLivewire3/resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
         </nav>
     </div>
 
-    {{ $slot }}
+    @yield('content')
 
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
