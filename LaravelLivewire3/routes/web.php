3b11c908d3a28c4f962ad2a2cacf1e2ee86d14b4
franciscusvalentinus <franciscusvalentinus@gmail.com>
2025-05-20 19:55:07 +0700

    Update web.php

--- LaravelLivewire3/routes/web.php
+++ LaravelLivewire3/routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//posts index
-// Route::get('/', App\Livewire\Posts\Index::class)->name('posts.index');
-
-//posts create
-// Route::get('/create', App\Livewire\Posts\Create::class)->name('posts.create');
-
-//posts edit
-// Route::get('/edit/{id}', App\Livewire\Posts\Edit::class)->name('posts.edit');
+Route::livewire('/', 'post.index')->name('post.index');
+Route::livewire('/create', 'post.create')->name('post.create');
+Route::livewire('/edit/{id}', 'post.edit')->name('post.edit');
